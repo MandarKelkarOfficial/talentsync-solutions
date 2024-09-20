ffdaa455fd6b962f893f7d8fb38ca82b71f71cdb
MADDY <mandarkelkarofficial@gmail.com>
2024-09-20 11:09:09 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// ScrapeCourses.js
 
-function App() {
+import React from 'react'
+import Scrper from './Components/Scrper'
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ <Scrper/>
+  )
 }
 
-export default App;
+
